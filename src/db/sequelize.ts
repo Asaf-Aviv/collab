223e987d4036dd2c795800352c8de6e82788af6e
Asaf Aviv <asafaviv89@gmail.com>
2019-10-09 10:49:29 +0300

    configuration

--- src/db/sequelize.ts
+++ src/db/sequelize.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize-typescript';
 
 export default new Sequelize(
-  'collab',
+  process.env.PG_DB as string,
   process.env.PG_USER as string,
   process.env.PG_PASS,
   {
@@ -13,9 +13,17 @@ export default new Sequelize(
       acquire: 30000,
       idle: 10000,
     },
-    logging: true,
+    logging: process.env.NODE_ENV !== 'production'
+      ? (sql: string) => {
+        console.log(`\x1b[36mQUERY: \n\x1b[33m${sql}\n`);
+        console.log('*'.repeat(20), '\n\u001b[0m');
+      }
+      : false,
     define: {
       underscored: true,
+    },
+    query: {
+      raw: true,
     },
   },
 );
