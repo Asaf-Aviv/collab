94b8d40e848d7409679751340bf442381df333ab
Asaf Aviv <asafaviv89@gmail.com>
2020-02-29 19:17:32 +0200

    added collab members/invitations

--- server/src/db/seeders/20200227093945-collab.js
+++ server/src/db/seeders/20200227093945-collab.js
@@ -28,11 +28,75 @@ const asafCollab = {
 }
 
 const seededCollabs = [...Array(10)].map(generageCollab).concat(asafCollab)
+const owners = seededCollabs.map(({ id, owner_id }) => ({
+  collab_id: id,
+  is_owner: true,
+  member_id: owner_id,
+  updated_at: new Date(),
+  created_at: new Date(),
+}))
+
+const comments = _.flatten(
+  seededCollabs.map(({ id }) =>
+    [...Array(15)].map(() => ({
+      id: uuid(),
+      collab_id: id,
+      author_id: _.sample(seededUsers).id,
+      content: faker.lorem.words(_.random(5, 14)),
+      updated_at: new Date(),
+      created_at: new Date(),
+    })),
+  ),
+)
+
+const memberInvitations = _.flatten(
+  seededCollabs.map(({ id, owner_id }) =>
+    _.take(
+      _.filter(seededUsers, ({ id }) => id !== owner_id),
+      _.random(1, 4),
+    ).map(({ id: userId }) => ({
+      collab_id: id,
+      type: 'invitation',
+      member_id: userId,
+      updated_at: new Date(),
+      created_at: new Date(),
+    })),
+  ),
+)
+
+const memberRequests = _.flatten(
+  seededCollabs.map(({ id: collab_id, owner_id }) =>
+    _.take(
+      _.filter(
+        seededUsers,
+        ({ id }) =>
+          id !== owner_id &&
+          !memberInvitations.find(
+            invite => invite.collab_id === collab_id && invite.member_id === id,
+          ),
+      ),
+      _.random(1, 4),
+    ).map(({ id: userId }) => ({
+      collab_id,
+      type: 'request',
+      member_id: userId,
+      updated_at: new Date(),
+      created_at: new Date(),
+    })),
+  ),
+)
+
+const allInvites = memberInvitations.concat(memberRequests)
 
 module.exports = {
   up: queryInterface =>
     queryInterface
       .bulkInsert({ tableName: 'collabs' }, seededCollabs)
+      .then(() => queryInterface.bulkInsert({ tableName: 'collab_members' }, owners))
+      .then(() => queryInterface.bulkInsert({ tableName: 'collab_comments' }, comments))
+      .then(() =>
+        queryInterface.bulkInsert({ tableName: 'collab_member_requests' }, allInvites),
+      )
       .catch(err => {
         console.log(err.message)
       }),
