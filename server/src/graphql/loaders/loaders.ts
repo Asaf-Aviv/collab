a196455a0dd66c77b75ccef4e9c40b3aaaead553
Asaf Aviv <asafaviv89@gmail.com>
2020-02-29 19:19:04 +0200

    added collab loader

--- server/src/graphql/loaders/loaders.ts
+++ server/src/graphql/loaders/loaders.ts
@@ -1,4 +1,5 @@
 import Dataloader from 'dataloader'
+import { Collab } from '../../db/models/Collab'
 import { User } from '../../db/models/User'
 import { find } from 'lodash'
 
@@ -8,6 +9,13 @@ const createUserLoader = () =>
     return ids.map(id => find(users, { id }) || null)
   })
 
+const createCollabLoader = () =>
+  new Dataloader<string, Collab | null>(async ids => {
+    const users = await Collab.findAll({ where: { id: ids as string[] } })
+    return ids.map(id => find(users, { id }) || null)
+  })
+
 export const createLoaders = () => ({
   userLoader: createUserLoader(),
+  collabLoader: createCollabLoader(),
 })
