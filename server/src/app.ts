70507df854b7786f7c1c65417b42366dc0d717f7
Asaf Aviv <asafaviv89@gmail.com>
2020-02-29 19:18:51 +0200

    added middlewares

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,35 +1,27 @@
 import express from 'express'
-import { ApolloServer } from 'apollo-server-express'
+import { ApolloServer, makeExecutableSchema } from 'apollo-server-express'
+import { applyMiddleware } from 'graphql-middleware'
 import typeDefs from './graphql/typeDefs'
+import { apolloContext } from './graphql/context/CollabContext'
+import { permissions } from './graphql/middleware/permissions'
 import resolvers from './graphql/resolvers'
-import { decodeToken } from './utils'
-import { User } from './db/models/User'
-import { createLoaders } from './graphql/loaders/loaders'
 
 export const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+const schema = applyMiddleware(
+  makeExecutableSchema({
+    typeDefs,
+    resolvers,
+  }),
+  permissions,
+)
+
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: async ({ req }) => {
-    try {
-      const token = (req.headers.authorization || '').replace('Bearer ', '')
-      const { userId } = await decodeToken(token)
-      const user = await User.findByPk(userId)
-      return {
-        ...createLoaders(),
-        user,
-      }
-    } catch (err) {
-      return {
-        ...createLoaders(),
-        user: null,
-      }
-    }
-  },
+  schema,
+  context: apolloContext,
 })
 
 server.applyMiddleware({ app })
