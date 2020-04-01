55a55ddaa82300061bb6bc8517eb3e0317c35425
Asaf Aviv <asafaviv89@gmail.com>
2020-04-02 02:30:35 +0300

    added collab post by stack

---
+++ client/src/components/CollabPostCard/CollabPostCard.tsx
@@ -0,0 +1,87 @@
+import React from 'react'
+import { CollabPostsQuery } from '../../graphql/generates'
+import { Link as RouterLink } from 'react-router-dom'
+import { Flex, Heading, Tag, Link, Text } from '@chakra-ui/core'
+import { AvatarWithUsername } from '../AvatarWithUsername/AvatarWithUsername'
+import { Paper } from '../global'
+import { CommentsAndReactionsCount } from '../CommentsAndReactionsCount/CommentsAndReactionsCount'
+import { formatDate } from '../../utils'
+import { PostStackTag } from '../PostStackTag/PostStackTag'
+
+export const CollabPostCard = ({
+  id,
+  title,
+  stack,
+  experience,
+  languages,
+  reactionsCount,
+  commentsCount,
+  createdAt,
+  isNew,
+  membersCount,
+  // hasStarted,
+  owner,
+}: NonNullable<CollabPostsQuery['collabPosts']>['posts'][0]) => (
+  <Paper h="100%" direction="column" align="start" as="article">
+    <Flex p={4} w="100%" align="center" justify="space-between">
+      <Flex align="center">
+        <AvatarWithUsername size="sm" {...owner} />
+        <Text ml={2} as="time" fontSize="0.9rem">
+          {formatDate(createdAt)}
+        </Text>
+        <Text ml={2} fontSize="0.9rem">
+          {`${membersCount} ${membersCount > 1 ? 'members' : 'member'}`}
+        </Text>
+      </Flex>
+      <Flex align="center">
+        <Tag boxShadow="sm" mr={2} size="sm" variantColor="pink">
+          {experience}
+        </Tag>
+        {isNew && (
+          <Tag boxShadow="sm" size="sm" variantColor="green">
+            NEW
+          </Tag>
+        )}
+      </Flex>
+    </Flex>
+    <Link
+      as={RouterLink as any}
+      // @ts-ignore
+      to={`/collabs/posts/${id}`}
+      h="100%"
+      px={4}
+      key={id}
+      w="100%"
+      flex={1}
+      zIndex={1}
+      _hover={{ textDecoration: 'none' }}
+    >
+      <Heading size="md" fontWeight={500} pb={6} as="h2">
+        {title}
+      </Heading>
+    </Link>
+    <Flex bg="#f2f2ff" p={4} w="100%">
+      {languages.map(language => (
+        <Tag
+          boxShadow="sm"
+          size="sm"
+          variantColor="purple"
+          key={language}
+          mr={2}
+        >
+          {language}
+        </Tag>
+      ))}
+      {stack.map(tech => (
+        <PostStackTag key={tech} mr={2}>
+          {tech}
+        </PostStackTag>
+      ))}
+      <CommentsAndReactionsCount
+        ml="auto"
+        reactionsCount={reactionsCount}
+        commentsCount={commentsCount}
+      />
+    </Flex>
+  </Paper>
+)
--- client/src/components/CollabPostsByStack/CollabPostsByStack.tsx
+++ client/src/components/CollabPostsByStack/CollabPostsByStack.tsx
@@ -1,20 +1,63 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import { useCollabPostsByStackQuery } from '../../graphql/generates'
+import { Container } from '../global'
+import { CollabPostCard } from '../CollabPostCard/CollabPostCard'
+import { SimpleGrid } from '@chakra-ui/core'
 
-type Props = {}
-
-export const CollabPostsByStack = (props: Props) => {
+export const CollabPostsByStack = () => {
   const { stack } = useParams<{ stack: string }>()
-  const { data } = useCollabPostsByStackQuery({
+  const { data, loading, error, fetchMore } = useCollabPostsByStackQuery({
     variables: {
       stack,
       offset: 0,
       limit: 10,
     },
   })
+  if (loading) return <h1>loading</h1>
+  if (error) return <h1>Could not fetch Collabs</h1>
+  if (!data?.collabPostsByStack) return null
 
+  const { posts, hasNextPage } = data.collabPostsByStack
   console.log(data)
 
-  return <div></div>
+  return (
+    <main>
+      <Container>
+        <SimpleGrid as="section" columns={{ lg: 1, xl: 2 }} spacing={6}>
+          {posts.map(post => (
+            <CollabPostCard key={post.id} {...post} />
+          ))}
+          {hasNextPage && (
+            <button
+              onClick={() =>
+                fetchMore({
+                  variables: {
+                    offset: posts.length,
+                    limit: 10,
+                  },
+                  updateQuery: (prev, { fetchMoreResult }) => {
+                    if (!fetchMoreResult) return prev
+                    return {
+                      collabPostsByStack: {
+                        ...prev.collabPostsByStack,
+                        hasNextPage:
+                          fetchMoreResult.collabPostsByStack.hasNextPage,
+                        posts: [
+                          ...prev.collabPostsByStack.posts,
+                          ...fetchMoreResult.collabPostsByStack.posts,
+                        ],
+                      },
+                    }
+                  },
+                })
+              }
+            >
+              fetch more
+            </button>
+          )}
+        </SimpleGrid>
+      </Container>
+    </main>
+  )
 }
