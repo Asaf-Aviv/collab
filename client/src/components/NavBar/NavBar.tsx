ee2414ea3bd96400e980bfa071abb561f4c03cb6
Asaf Aviv <asafaviv89@gmail.com>
2020-04-10 14:42:15 +0300

    added friend request popover

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import { NavLink, Link } from 'react-router-dom'
 import {
   Flex,
@@ -12,8 +12,13 @@ import {
   Avatar,
 } from '@chakra-ui/core'
 import styled from 'styled-components'
+import NotificationsRoundedIcon from '@material-ui/icons/NotificationsRounded'
+import GroupAddIcon from '@material-ui/icons/GroupAdd'
 import { useApolloClient } from '@apollo/react-hooks'
-import { useGetCurrentUserQuery } from '../../graphql/generates'
+import {
+  useGetCurrentUserQuery,
+  useCurrentUserFriendRequestsQuery,
+} from '../../graphql/generates'
 import { Container } from '../global'
 
 export const NavBar = () => {
@@ -45,7 +50,10 @@ export const NavBar = () => {
           <StyledLink to="/create">Create</StyledLink>
         </StyledFlex>
         {currentUser ? (
-          <UserNavMenu />
+          <Flex>
+            <UserNavPanel />
+            <UserNavMenu />
+          </Flex>
         ) : (
           <StyledFlex as="div">
             <NavLink to="/login">Login</NavLink>
@@ -74,6 +82,63 @@ const StyledLink = styled(NavLink)`
 
   & + & {
     margin-left: 1rem;
+  }
+`
+
+const UserNavPanel = () => {
+  return (
+    <Flex>
+      <Flex>
+        <NotificationsRoundedIcon />
+      </Flex>
+      <FriendRequestsModal />
+    </Flex>
+  )
+}
+
+const FriendRequestsModal = () => {
+  const [isFriendRequestsOpen, toggleIsFriendRequestsOpen] = useReducer(
+    open => !open,
+    false,
+  )
+  const { data, loading, error } = useCurrentUserFriendRequestsQuery()
+
+  if (loading) return <span>loading...</span>
+  if (error) return <span>Could not fetch friend requests</span>
+  if (!data?.currentUser) return null
+
+  const { friendRequests } = data.currentUser
+
+  return (
+    <Flex position="relative">
+      <Flex height="100%" align="center">
+        <IconButton onClick={() => toggleIsFriendRequestsOpen()}>
+          <GroupAddIcon />
+        </IconButton>
+      </Flex>
+      {isFriendRequestsOpen && (
+        <Flex direction="column" position="absolute" bottom={0}>
+          {friendRequests.map(user => (
+            <Flex key={user.id}>{user.username}</Flex>
+          ))}
+        </Flex>
+      )}
+    </Flex>
+  )
+}
+
+const IconButton = styled.button`
+  height: 2rem;
+  width: 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 4px;
+  transition: background-color 200ms;
+  margin-left: 0.5rem;
+  margin-right: 0.5rem;
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.05);
   }
 `
 
