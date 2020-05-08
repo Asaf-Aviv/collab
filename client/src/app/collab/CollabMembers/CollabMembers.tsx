13deb6c425e0c1de14ba2da4f3d658b320df9b1a
Asaf Aviv <asafaviv89@gmail.com>
2020-05-08 15:18:31 +0300

    collab ducks

---
+++ client/src/app/collab/CollabMembers/CollabMembers.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import { useCollabMembersQuery } from '../../../graphql/generates'
+import { UserCard } from '../../../components/UserCard/UserCard'
+import { Grid } from '@chakra-ui/core'
+
+export const CollabMembers = () => {
+  const { collabId } = useParams<{ collabId: string }>()
+  const { data } = useCollabMembersQuery({ variables: { collabId } })
+
+  return (
+    <Grid
+      gap={4}
+      templateColumns="repeat(auto-fill, minMax(300px, 350px))"
+      justifyContent={{ sm: 'center', lg: 'initial' }}
+    >
+      {data?.collab?.members.map(member => (
+        <UserCard key={member.id} {...member} />
+      ))}
+    </Grid>
+  )
+}
