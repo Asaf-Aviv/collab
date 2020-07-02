7da173c2d85252f1c9b09fd6333f5858c281cb10
Asaf-Aviv <asafaviv89@gmail.com>
2020-07-02 14:17:44 +0300

    sequelize instance

---
+++ server/src/db/sequelize.ts
@@ -0,0 +1,30 @@
+import { Sequelize, SequelizeOptions } from 'sequelize-typescript'
+import { models } from './models'
+
+const baseConfig: SequelizeOptions = {
+  models: Object.values(models),
+  dialect: 'postgres',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+  logging: false,
+  define: {
+    underscored: true,
+  },
+}
+
+const dbByEnv = {
+  development: 'collab_dev',
+  test: 'collab_test',
+  production: 'collab',
+}
+
+export default new Sequelize(
+  dbByEnv[process.env.NODE_ENV as keyof typeof dbByEnv],
+  'root',
+  '',
+  baseConfig,
+)
