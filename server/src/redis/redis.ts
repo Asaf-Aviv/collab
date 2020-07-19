d68c8085ed5d66580a95d1b0a684b500d0252f50
Asaf Aviv <asafaviv89@gmail.com>
2020-07-19 19:27:22 +0300

    small changes

--- server/src/redis/redis.ts
+++ server/src/redis/redis.ts
@@ -4,8 +4,4 @@ export const redis = new Redis({
   showFriendlyErrorStack: process.env.NODE_ENV !== 'production',
 })
 
-redis.flushall().catch(err => {
-  console.log('flush all error', err)
-})
-
 export const REDIS_CHAT_USERS = 'chat:users:'
--- server/src/server.ts
+++ server/src/server.ts
@@ -3,21 +3,23 @@ require('dotenv').config()
 import { createServer } from 'http'
 import sequelize from './db/sequelize'
 import { app, apolloServer } from './app'
-import { models } from './db/models'
-import { pubsub } from './graphql/helpers/pubsub'
+import { redis } from './redis/redis'
 
 const PORT = 5555
 
 const httpServer = createServer(app)
 apolloServer.installSubscriptionHandlers(httpServer)
 
-sequelize
-  .authenticate()
-  .then(async () => {
-    // await sequelize.sync({ force: true })
+redis
+  .flushall()
+  .then(() =>
+    sequelize.authenticate().then(async () => {
+      // await sequelize.sync({ force: true })
 
-    httpServer.listen(PORT, () =>
-      console.info(`Server running on port ${PORT}`),
-    )
-  })
+      httpServer.listen(PORT, () =>
+        console.info(`Server running on port ${PORT}`),
+      )
+    }),
+  )
+
   .catch(console.log)
