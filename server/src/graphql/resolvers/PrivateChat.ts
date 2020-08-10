88ccf01d35625da579630674910e11d741f53ca1
Asaf Aviv <asafaviv89@gmail.com>
2020-08-10 15:10:58 +0300

    added middleware

--- server/src/graphql/resolvers/PrivateChat.ts
+++ server/src/graphql/resolvers/PrivateChat.ts
@@ -1,17 +1,43 @@
-import { pubsub } from '../helpers/pubsub'
-import { withFilter } from 'apollo-server-express'
+import IORedis from 'ioredis'
+import { and } from 'graphql-shield'
 import { v1 as uuid } from 'uuid'
+import { withFilter } from 'apollo-server-express'
+import { pubsub } from '../helpers/pubsub'
 import { Resolvers, UserChatStatus } from '../types.d'
 import { isNotNull, withCancel } from '../../helpers/helpers'
 import { REDIS_CHAT_USERS } from '../../redis/redis'
+import { isAuthenticated } from '../middleware/isAuthenticated'
+import { User } from '../../db/models/User'
 
 const NEW_PRIVATE_CHAT_MESSAGE = 'NEW_PRIVATE_CHAT_MESSAGE'
 const FRIEND_STATUS_CHANGE = 'FRIEND_STATUS_CHANGE'
 
+const notifyChatUsersOnStatusChange = (
+  user: User,
+  status: UserChatStatus,
+  redis: IORedis.Redis,
+) => {
+  User.getAllUserFriends(user.id).then(friends => {
+    friends.forEach(async friend => {
+      const friendStatus = await redis.hget(REDIS_CHAT_USERS, friend.id)
+
+      if (friendStatus === null) return
+
+      pubsub.publish(FRIEND_STATUS_CHANGE, {
+        recipientId: friend.id,
+        friendStatusChange: {
+          user,
+          status,
+        },
+      })
+    })
+  })
+}
+
 export const privateChatResolver: Resolvers = {
-  Mutation: {
-    connectToChat: async (root, { status }, { user, models, redis }) => {
-      await redis.hset(REDIS_CHAT_USERS, user!.id, status)
+  Query: {
+    onlineChatFriends: async (root, args, { user, models, redis }) => {
+      await redis.hset(REDIS_CHAT_USERS, user!.id, 'ONLINE')
 
       const allFriends = await models.User.getAllUserFriends(user!.id)
 
@@ -25,7 +51,7 @@ export const privateChatResolver: Resolvers = {
             recipientId: friend.id,
             friendStatusChange: {
               user,
-              status,
+              status: 'ONLINE',
             },
           })
 
@@ -38,6 +64,8 @@ export const privateChatResolver: Resolvers = {
       const users = allFriendStatuses.filter(isNotNull)
       return { users }
     },
+  },
+  Mutation: {
     updateStatus: async (root, { status }, { models, redis, user }) => {
       await redis.hset(REDIS_CHAT_USERS, user!.id, status)
 
@@ -85,11 +113,16 @@ export const privateChatResolver: Resolvers = {
           ({ recipientId }) => recipientId === user!.id,
         )
 
-        return withCancel(subscriptionFilter(), () =>
-          redis.hdel(REDIS_CHAT_USERS, user!.id).catch(err => {
-            console.error('Deleting connected user error', err)
-          }),
-        )
+        return withCancel(subscriptionFilter(), () => {
+          redis
+            .hdel(REDIS_CHAT_USERS, user!.id)
+            .catch(err => {
+              console.error('Deleting connected chat user error', err)
+            })
+            .finally(() => {
+              notifyChatUsersOnStatusChange(user!, 'OFFLINE', redis)
+            })
+        })
       },
     },
     friendStatusChange: {
@@ -99,5 +132,19 @@ export const privateChatResolver: Resolvers = {
           ({ recipientId }) => recipientId === user!.id,
         )(),
     },
+  },
+}
+
+export const privateChatMiddleware = {
+  Query: {
+    onlineChatFriends: and(isAuthenticated),
+  },
+  Mutation: {
+    updateStatus: and(isAuthenticated),
+    sendPrivateChatMessage: and(isAuthenticated),
+  },
+  Subscription: {
+    newPrivateChatMessage: and(isAuthenticated),
+    friendStatusChange: and(isAuthenticated),
   },
 }
